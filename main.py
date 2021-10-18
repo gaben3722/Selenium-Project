10a0225735275d2ff156eaa416890f8e08105dc9
gaben3722 <gabenosek18@yahoo.com>
2021-10-17 23:58:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import selenium
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.reddit.com/")
+
+
+search = driver.find_element_by_name("")
+
 driver.quit()

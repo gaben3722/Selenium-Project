01a99a0a82b165c7e9d89f06ccd765165d4ff09b
gaben3722 <gabenosek18@yahoo.com>
2021-10-24 20:44:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.reddit.com/")
 
 
-search = driver.find_element_by_name("")
+search = driver.find_element_by_name("wallstreetbets")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
 
 driver.quit()

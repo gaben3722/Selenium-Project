4697c953d632ffa5c49eff1dd5d61a541bee99ca
gaben3722 <gabenosek18@yahoo.com>
2021-10-25 20:45:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ search = driver.find_element_by_name("wallstreetbets")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
-time.sleep(5)
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+except:
+    driver.quit()
+
+print(main.text)
 
 driver.quit()
